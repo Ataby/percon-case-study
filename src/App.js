103c2bb5ee143381227fe3ef1888d60dedfb27e2
Ataby <matabay200069@gmail.com>
2024-08-23 23:52:26 +0300

    form page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/Form";
+import Listing from "./components/Listing";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-      Form component
+    <div class="flex">
+      <div className="border-2 border-red-400 w-1/2 max-h-full">
+        <Form />
       </div>
 
-      <div>
-      Listing component
-      </div>
+      <div class="w-1/2 border-2 border-blue-400 max-h-full">Listing component</div>
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import Box from "@mui/material/Box";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import EventNoteIcon from "@mui/icons-material/EventNote";
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-export default Form
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      sx={{
+        padding: "10px",
+        height: "100vh",
+      }}
+    >
+      <Autocomplete
+        disablePortal
+        onChange={(event, newValue) => {
+            setSelectedCategory(newValue);
+            console.log(newValue) // Seçilen değeri günceller
+          }}
+  
+        id="combo-box-demo"
+        options={noteCategories}
+        sx={{ minWidth: "70%", padding: "10px" }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            sx={{ bgcolor: "rgba(170, 200, 235, 0.9)" }}
+            label="Categories"
+          />
+        )}
+        renderOption={(props, option) => {
+          console.log(option.icon);
+          return (
+            <Box
+              component="li"
+              sx={{ "& > img": { ml: 8, flexShrink: 0 } }}
+              {...props}
+            >
+              {option.icon} {option.label}
+            </Box>
+          );
+        }}
+      />
+
+      <TextField
+        id="outlined-multiline-static"
+        margin="none"
+        placeholder="Title"
+        label="Title"
+        rows={2}
+        multiline
+        sx={{
+          minWidth: "70%",
+          marginTop: "2rem",
+        }}
+        onChange={(e)=> {
+            setTitle(e.target.value);
+        }}
+      />
+
+      <TextField
+        id="outlined-multiline-static"
+        label="Content"
+        placeholder="Content"
+        multiline
+        rows={10}
+        sx={{
+          minWidth: "70%",
+          marginTop: "2rem",
+        }}
+        onChange={(e)=> {
+            setContent(e.target.value);
+        }}
+      />
+
+      <Button
+        onClick={() => {
+          alert("Succesfully Added");
+        }}
+        variant="contained"
+        size="large"
+        endIcon={<SendIcon />}
+        color="primary"
+        sx={{ width: "30%", marginTop: "3rem" }}
+      >
+        Submit
+      </Button>
+    </Box>
+  );
+};
+const noteCategories = [
+  { label: "Daily Routines", icon: <AccessTimeIcon /> },
+  { label: "Weekly Routines", icon: <DateRangeIcon /> },
+  { label: "Monthly Routines", icon: <EventNoteIcon /> },
+  { label: "Special Events", icon: <StarRateIcon /> },
+];
+
+export default Form;
